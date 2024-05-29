8c10d1354a170f2a2fa5935de7099b3453d661b6
Catalin Mihai Popoiu <117656515+MihaiCatalin20@users.noreply.github.com>
2024-05-29 22:31:27 +0200

    Update App.jsx

--- individual-assignment-semester-3-fe/src/App.jsx
+++ individual-assignment-semester-3-fe/src/App.jsx
@@ -8,7 +8,7 @@ import RegisterPage from './components/RegisterPage';
 import ForumPage from './components/forum/ForumPage';
 import ForumPostDetails from './components/forum/ForumPostDetails';
 import GamesPage from './components/game/GamesPage';
-import UserList from './components/user/UserList';
+import UserList from './components/user/UserList.jsx';
 import UserDetailPage from './components/user/UserDetailPage';
 import GameForm from './components/game/GameForm';
 import GameDetailsPage from './components/game/GameDetails';
